2694ed92d3913e27fd5aa33713c48f150a5282c5
tanjing <526211447@qq.com>
2018-12-06 01:15:57 +0800

    update

--- src/main/java/com/fen/FenController.java
+++ src/main/java/com/fen/FenController.java
@@ -2,13 +2,12 @@ package com.fen;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class FenController {
-    @RequestMapping(path={"/index","/"})
-    @ResponseBody
+    @RequestMapping("/ok")
+//    @ResponseBody
     public String lovefen(){
-        return "Love Fen";
+        return "index";
     }
 }
